090e2afc839a9bbc1298bd2b7bc8bc7bd92d0e16
Aravind Dasarathy <dasarathy.ar@northeastern.edu>
2023-02-11 19:43:14 -0500

    implementation for hwqupc

--- src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUPC.java
+++ src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUPC.java
@@ -82,7 +82,18 @@ public class UF_HWQUPC implements UF {
         validate(p);
         int root = p;
         // FIXME
-        // END 
+        while (root != parent[root]) {
+            root = parent[root];
+        }
+
+        if (pathCompression) {
+            while (p != root) {
+                int newp = parent[p];
+                parent[p] = root;
+                p = newp;
+            }
+        }
+        // END
         return root;
     }
 
@@ -170,7 +181,14 @@ public class UF_HWQUPC implements UF {
 
     private void mergeComponents(int i, int j) {
         // FIXME make shorter root point to taller one
-        // END 
+        if (height[i] < height[j]) {
+            updateParent(i, j);
+            updateHeight(j, i);
+        } else {
+            updateParent(j, i);
+            updateHeight(i, j);
+        }
+        // END
     }
 
     /**
@@ -178,6 +196,10 @@ public class UF_HWQUPC implements UF {
      */
     private void doPathCompression(int i) {
         // FIXME update parent to value of grandparent
+        while (i != parent[i]) {
+            parent[i] = parent[parent[i]];
+            i = parent[i];
+        }
         // END 
     }
 }
